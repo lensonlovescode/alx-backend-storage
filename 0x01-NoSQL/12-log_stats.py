01f32b06a587ed2b66eb401b9a8ff4e7cc028ee8
lensonlovescode <lensonisinparis@gmail.com>
2025-06-21 15:34:29 +0300

    NGINX summary & pycodestyle

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,18 +5,22 @@ Contains a script that provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
-client = MongoClient()
-collection = client.logs.nginx
+if __name__ == '__main__':
+    """
+    Summarizes nginx logs stored in the database
+    """
+    client = MongoClient()
+    collection = client.logs.nginx
 
-listOfMethods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    listOfMethods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-logs = collection.count_documents({})
-print(f"{logs} logs")
+    logs = collection.count_documents({})
+    print(f"{logs} logs")
 
-print("Methods:")
-for method in listOfMethods:
-    count = collection.count_documents({"method": method})
-    print(f"    method {method}: {count}")
+    print("Methods:")
+    for method in listOfMethods:
+        count = collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
 
-status = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"{status} status check")
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
